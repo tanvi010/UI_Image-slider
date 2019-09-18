19bae454d12bd8c734f33b5dcacb3556b77df950
COE-030 <coe-030@coe-030s-imac.local>
2019-09-18 15:34:38 +0530

    Image slider..

--- UI_ImageSlider/ViewController.swift
+++ UI_ImageSlider/ViewController.swift
@@ -8,18 +8,87 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var NextHide: UIButton!
+    
+    @IBOutlet weak var PreviousHide: UIButton!
+    
+    
+    var Image = 0
+    @IBAction func Previous(_ sender: Any)
+    {
+        Image -= 1
+        Label.text = String("\(Image)")
+        self.ImageGallery()
+        
     }
+    
+    @IBAction func Next(_ sender: Any)
+    {
+        Image += 1
+        Label.text = String("\(Image)")
+        self.ImageGallery()
+    }
+    
+    @IBOutlet weak var Label: UILabel!
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        Image = 1
+        Label.text = String("\(Image)")
+    }
+
+        
+        // Do any additional setup after loading the view, typically from a nib
+    
+    func ImageGallery()
+    {
+        
+        if Image == 1
+        {
+            PreviousHide.isEnabled = false
+            imageView.image = UIImage(named: "background1.jpeg")
+        }
+        
+        if Image == 2
+        {
+            PreviousHide.isEnabled = true
+            imageView.image = UIImage(named: "background2.jpeg")
+        }
+        
+        if Image == 3
+        {
+            PreviousHide.isEnabled = true
+            imageView.image = UIImage(named: "background3.jpeg")
+        }
+        
+        if Image == 4
+        {
+            NextHide.isEnabled = true
+            imageView.image = UIImage(named: "background4.jpeg")
+        }
+        
+        if Image == 5
+        {
+            NextHide.isEnabled = false
+            imageView.image = UIImage(named: "background5.jpeg")
+        }
+        
+}
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
+
+
+
 
